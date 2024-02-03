781e342a8330039f0cb28add2ace432dd70f129f
Remi Marche <remimarche@gmail.com>
2024-02-03 16:50:40 -0500

    visual fixes

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,6 @@
 <template>
-  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/ecstrema/hackatown-2024" target="_blank" title="Link to GitHub Repo">
-    <pixelarticons-power />
-  </a>
+  <div class="flex items-center justify-center">
+    <img src="../assets/icon.svg" class="icon-btn mx-2 text-2xl" alt="extension icon">
+    <div>Options</div>
+  </div>
 </template>
--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+import { showInPage } from '~/logic/storage'
 </script>
 
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <img src="/assets/icon.svg" class="icon-btn mx-2 text-2xl" alt="extension icon">
     <div>Options</div>
-    <SharedSubtitle />
 
-    <input v-model="storageDemo" type="checkbox" class="border border-gray-400 rounded px-2 py-1 mt-2">
-
-    <div class="mt-4">
-      Powered by Vite <pixelarticons-zap class="align-middle inline-block" />
+    <div class="flex gap-2 items-center justify-center">
+      <label for="showInPage">Show popup inside page: </label>
+      <input id="showInPage" v-model="showInPage" type="checkbox">
     </div>
   </main>
 </template>
